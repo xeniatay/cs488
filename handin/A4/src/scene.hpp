5f3865ae87a356ce01929fff2472257cc12bc038
Xenia Tay <xeniatzy@gmail.com>
2014-06-23 23:14:13 -0400

    grab scene files from A3

--- handin/A4/src/scene.hpp
+++ handin/A4/src/scene.hpp
@@ -5,98 +5,112 @@
 #include "algebra.hpp"
 #include "primitive.hpp"
 #include "material.hpp"
+#include <GL/gl.h>
+#include <GL/glu.h>
 
 class SceneNode {
-public:
-  SceneNode(const std::string& name);
-  virtual ~SceneNode();
+  public:
+    SceneNode(const std::string& name);
+    virtual ~SceneNode();
 
-  const Matrix4x4& get_transform() const { return m_trans; }
-  const Matrix4x4& get_inverse() const { return m_invtrans; }
-  
-  void set_transform(const Matrix4x4& m)
-  {
-    m_trans = m;
-    m_invtrans = m.invert();
-  }
+    virtual void walk_gl(bool picking = false);
 
-  void set_transform(const Matrix4x4& m, const Matrix4x4& i)
-  {
-    m_trans = m;
-    m_invtrans = i;
-  }
+    const Matrix4x4& get_transform() const { return m_trans; }
+    const Matrix4x4& get_inverse() const { return m_invtrans; }
 
-  void add_child(SceneNode* child)
-  {
-    m_children.push_back(child);
-  }
+    void set_transform(const Matrix4x4& m)
+    {
+      m_trans = m;
+      m_invtrans = m.invert();
+    }
 
-  void remove_child(SceneNode* child)
-  {
-    m_children.remove(child);
-  }
+    void set_transform(const Matrix4x4& m, const Matrix4x4& i)
+    {
+      m_trans = m;
+      m_invtrans = i;
+    }
 
-  // Callbacks to be implemented.
-  // These will be called from Lua.
-  void rotate(char axis, double angle);
-  void scale(const Vector3D& amount);
-  void translate(const Vector3D& amount);
+    void add_child(SceneNode* child)
+    {
+      m_children.push_back(child);
 
-  // Returns true if and only if this node is a JointNode
-  virtual bool is_joint() const;
-  
-protected:
-  
-  // Useful for picking
-  int m_id;
-  std::string m_name;
+      //std::cerr << "SceneNode " << this->m_name << " add child" << std::endl;
+    }
 
-  // Transformations
-  Matrix4x4 m_trans;
-  Matrix4x4 m_invtrans;
+    void remove_child(SceneNode* child)
+    {
+      m_children.remove(child);
+    }
 
-  // Hierarchy
-  typedef std::list<SceneNode*> ChildList;
-  ChildList m_children;
+    // Callbacks to be implemented.
+    // These will be called from Lua.
+    void rotate(char axis, double angle);
+    void scale(const Vector3D& amount);
+    void translate(const Vector3D& amount);
+
+    // Returns true if and only if this node is a JointNode
+    virtual bool is_joint();
+
+    // Useful for picking
+    int m_id;
+    int m_geo_id;
+    std::string m_name;
+
+    void printTM();
+
+    void gl_mult_trans();
+
+    // Transformations
+    Matrix4x4 m_trans;
+    Matrix4x4 m_invtrans;
+    Matrix4x4 m_rot;
+    GLfloat m_gl_trans[16];
+
+    // Hierarchy
+    typedef std::list<SceneNode*> ChildList;
+    ChildList m_children;
 };
 
 class JointNode : public SceneNode {
-public:
-  JointNode(const std::string& name);
-  virtual ~JointNode();
+  public:
+    JointNode(const std::string& name);
+    virtual ~JointNode();
 
-  virtual bool is_joint() const;
+    virtual void walk_gl(bool bicking = false);
 
-  void set_joint_x(double min, double init, double max);
-  void set_joint_y(double min, double init, double max);
+    virtual bool is_joint();
 
-  struct JointRange {
-    double min, init, max;
-  };
+    void set_joint_x(double min, double init, double max);
+    void set_joint_y(double min, double init, double max);
 
-  
-protected:
+    struct JointRange {
+      double min, init, max;
+    };
 
-  JointRange m_joint_x, m_joint_y;
+
+    JointRange m_joint_x, m_joint_y;
 };
 
 class GeometryNode : public SceneNode {
-public:
-  GeometryNode(const std::string& name,
-               Primitive* primitive);
-  virtual ~GeometryNode();
+  public:
+    GeometryNode(const std::string& name, Primitive* primitive);
+    virtual ~GeometryNode();
 
-  const Material* get_material() const;
-  Material* get_material();
+    virtual void walk_gl(bool picking = false);
 
-  void set_material(Material* material)
-  {
-    m_material = material;
-  }
+    const Material* get_material() const;
+    Material* get_material();
 
-protected:
-  Material* m_material;
-  Primitive* m_primitive;
+    void set_material(Material* material)
+    {
+      m_material = material;
+    }
+
+    Material* m_material;
+    Primitive* m_primitive;
+
+    void transform_node();
+
 };
 
 #endif

ac06d726fe4407cc6acdf5a2323f4d25a1a0cba0
Xenia Tay <xeniatzy@gmail.com>
2014-06-25 05:12:54 -0400

    putting orig scene files back

--- handin/A4/src/scene.cpp
+++ handin/A4/src/scene.cpp
@@ -1,108 +1,48 @@
 #include "scene.hpp"
 #include <iostream>
-#include "a2.hpp"
-#include <vector>
 
-using std::vector;
-using std::cerr;
-using std::endl;
-
-vector<SceneNode*> scenenodes_v;
-
-typedef std::list<SceneNode*> SN;
-SN all_scenenodes;
-
-typedef std::list<SceneNode*> GN;
-GN all_geonodes;
-
-SceneNode::SceneNode(const std::string& name) : m_name(name) {
-
-  this->m_id = all_scenenodes.size() + 1;
-  m_trans = Matrix4x4();
-
-  all_scenenodes.push_back(this);
-  scenenodes_v.push_back(this);
-
-  //cerr << "Init sceneNode: " << name << " " << this->m_id << endl;
+SceneNode::SceneNode(const std::string& name)
+  : m_name(name)
+{
 }
 
-SceneNode::~SceneNode() { }
-
-void SceneNode::walk_gl(bool picking)
+SceneNode::~SceneNode()
 {
-  //cerr << "SceneNode " << m_name << " Walk GL" << endl;
-
-  glPushMatrix();
-
-  //cerr << "m_trans and rot " << endl << m_trans << endl << m_rot << endl;
-
-  // Translate root
-  gl_mult_trans();
-
-  for( std::list<SceneNode*>::const_iterator i = m_children.begin(); i != m_children.end(); ++i ) {
-    SceneNode *node = (*i);
-    node->walk_gl();
-  }
-
-  glPopMatrix();
-
-  //cerr << "SceneNode " << m_name << " End Walk GL" << endl;
 }
 
 void SceneNode::rotate(char axis, double angle)
 {
   std::cerr << "Stub: Rotate " << m_name << " around " << axis << " by " << angle << std::endl;
-  m_rot = m_rot * rotation(angle, axis);
-  m_trans = m_trans * rotation(angle, axis);
+  // Fill me in
 }
 
 void SceneNode::scale(const Vector3D& amount)
 {
   std::cerr << "Stub: Scale " << m_name << " by " << amount << std::endl;
-  m_trans = m_trans * scaling(amount);
+  // Fill me in
 }
 
 void SceneNode::translate(const Vector3D& amount)
 {
   std::cerr << "Stub: Translate " << m_name << " by " << amount << std::endl;
-  m_trans = m_trans * translation(amount);
+  // Fill me in
 }
 
-bool SceneNode::is_joint()
+bool SceneNode::is_joint() const
 {
   return false;
 }
 
-void SceneNode::printTM() {
-  int i = 0;
-
-  for (i = 0; i < 16; i++) {
-    cerr << m_gl_trans[i] << ", " << endl;
-  }
-
-}
-
-JointNode::JointNode(const std::string& name) : SceneNode(name) {
+JointNode::JointNode(const std::string& name)
+  : SceneNode(name)
+{
 }
 
 JointNode::~JointNode()
 {
 }
 
-void JointNode::walk_gl(bool picking)
-{
-  glPushMatrix();
-  gl_mult_trans();
-
-  for( std::list<SceneNode*>::const_iterator i = m_children.begin(); i != m_children.end(); ++i ) {
-    SceneNode *node = (*i);
-    node->walk_gl();
-  }
-
-  glPopMatrix();
-}
-
-bool JointNode::is_joint()
+bool JointNode::is_joint() const
 {
   return true;
 }
@@ -121,69 +61,13 @@ void JointNode::set_joint_y(double min, double init, double max)
   m_joint_y.max = max;
 }
 
-GeometryNode::GeometryNode(const std::string& name, Primitive* primitive) : SceneNode(name), m_primitive(primitive) {
-
-  //cerr << "Init geometryNode: " << name << endl;
-
-  this->m_primitive = primitive;
-  this->m_name = name;
-
-  this->m_geo_id = all_geonodes.size();
-  all_geonodes.push_back(this);
-
-}
-
-GeometryNode::~GeometryNode() {
-}
-
-void GeometryNode::walk_gl(bool picking)
+GeometryNode::GeometryNode(const std::string& name, Primitive* primitive)
+  : SceneNode(name),
+    m_primitive(primitive)
 {
-
-  //cerr << "GeometryNode " << m_name << " Walk GL" << endl;
-
-  if (m_material) {
-    m_material->apply_gl();
-  }
-
-  //cerr << "GeometryNode " << m_name << " End Walk GL" << endl;
-
-
-  glPushMatrix();
-  gl_mult_trans();
-
-  this->m_primitive->walk_gl(false);
-  //cerr << m_name << "'s sphere was drawn" << endl;
-
-  for( std::list<SceneNode*>::const_iterator i = m_children.begin(); i != m_children.end(); ++i ) {
-    SceneNode *node = (*i);
-    node->walk_gl();
-  }
-
-  glPopMatrix();
-
 }
 
-// make the transform matrix into a gl_float
-void SceneNode::gl_mult_trans() {
-
-  Matrix4x4 tr = m_trans;
-
-  int i = 0, j = 0, index;
-
-  for (i = 0; i < 4; i++) {
-    for (j = 0; j < 4; j++) {
-      index = (i * 4) + j;
-      m_gl_trans[index] = tr[i][j];
-    }
-  }
-
-  glMultTransposeMatrixf(m_gl_trans);
-
-  //cerr << m_name << "'s GL transform matrix has been set:" << endl << tr << endl;
+GeometryNode::~GeometryNode()
+{
 }
-
-// apply transformations
-void GeometryNode::transform_node() {
-
-
-}
+ 
--- handin/A4/src/scene.hpp
+++ handin/A4/src/scene.hpp
@@ -5,112 +5,98 @@
 #include "algebra.hpp"
 #include "primitive.hpp"
 #include "material.hpp"
-#include <GL/gl.h>
-#include <GL/glu.h>
 
 class SceneNode {
-  public:
-    SceneNode(const std::string& name);
-    virtual ~SceneNode();
+public:
+  SceneNode(const std::string& name);
+  virtual ~SceneNode();
 
-    virtual void walk_gl(bool picking = false);
+  const Matrix4x4& get_transform() const { return m_trans; }
+  const Matrix4x4& get_inverse() const { return m_invtrans; }
+  
+  void set_transform(const Matrix4x4& m)
+  {
+    m_trans = m;
+    m_invtrans = m.invert();
+  }
 
-    const Matrix4x4& get_transform() const { return m_trans; }
-    const Matrix4x4& get_inverse() const { return m_invtrans; }
+  void set_transform(const Matrix4x4& m, const Matrix4x4& i)
+  {
+    m_trans = m;
+    m_invtrans = i;
+  }
 
-    void set_transform(const Matrix4x4& m)
-    {
-      m_trans = m;
-      m_invtrans = m.invert();
-    }
+  void add_child(SceneNode* child)
+  {
+    m_children.push_back(child);
+  }
 
-    void set_transform(const Matrix4x4& m, const Matrix4x4& i)
-    {
-      m_trans = m;
-      m_invtrans = i;
-    }
+  void remove_child(SceneNode* child)
+  {
+    m_children.remove(child);
+  }
 
-    void add_child(SceneNode* child)
-    {
-      m_children.push_back(child);
+  // Callbacks to be implemented.
+  // These will be called from Lua.
+  void rotate(char axis, double angle);
+  void scale(const Vector3D& amount);
+  void translate(const Vector3D& amount);
 
-      //std::cerr << "SceneNode " << this->m_name << " add child" << std::endl;
-    }
+  // Returns true if and only if this node is a JointNode
+  virtual bool is_joint() const;
+  
+protected:
+  
+  // Useful for picking
+  int m_id;
+  std::string m_name;
 
-    void remove_child(SceneNode* child)
-    {
-      m_children.remove(child);
-    }
+  // Transformations
+  Matrix4x4 m_trans;
+  Matrix4x4 m_invtrans;
 
-    // Callbacks to be implemented.
-    // These will be called from Lua.
-    void rotate(char axis, double angle);
-    void scale(const Vector3D& amount);
-    void translate(const Vector3D& amount);
-
-    // Returns true if and only if this node is a JointNode
-    virtual bool is_joint();
-
-    // Useful for picking
-    int m_id;
-    int m_geo_id;
-    std::string m_name;
-
-    void printTM();
-
-    void gl_mult_trans();
-
-    // Transformations
-    Matrix4x4 m_trans;
-    Matrix4x4 m_invtrans;
-    Matrix4x4 m_rot;
-    GLfloat m_gl_trans[16];
-
-    // Hierarchy
-    typedef std::list<SceneNode*> ChildList;
-    ChildList m_children;
+  // Hierarchy
+  typedef std::list<SceneNode*> ChildList;
+  ChildList m_children;
 };
 
 class JointNode : public SceneNode {
-  public:
-    JointNode(const std::string& name);
-    virtual ~JointNode();
+public:
+  JointNode(const std::string& name);
+  virtual ~JointNode();
 
-    virtual void walk_gl(bool bicking = false);
+  virtual bool is_joint() const;
 
-    virtual bool is_joint();
+  void set_joint_x(double min, double init, double max);
+  void set_joint_y(double min, double init, double max);
 
-    void set_joint_x(double min, double init, double max);
-    void set_joint_y(double min, double init, double max);
+  struct JointRange {
+    double min, init, max;
+  };
 
-    struct JointRange {
-      double min, init, max;
-    };
+  
+protected:
 
-
-    JointRange m_joint_x, m_joint_y;
+  JointRange m_joint_x, m_joint_y;
 };
 
 class GeometryNode : public SceneNode {
-  public:
-    GeometryNode(const std::string& name, Primitive* primitive);
-    virtual ~GeometryNode();
+public:
+  GeometryNode(const std::string& name,
+               Primitive* primitive);
+  virtual ~GeometryNode();
 
-    virtual void walk_gl(bool picking = false);
+  const Material* get_material() const;
+  Material* get_material();
 
-    const Material* get_material() const;
-    Material* get_material();
+  void set_material(Material* material)
+  {
+    m_material = material;
+  }
 
-    void set_material(Material* material)
-    {
-      m_material = material;
-    }
-
-    Material* m_material;
-    Primitive* m_primitive;
-
-    void transform_node();
-
+protected:
+  Material* m_material;
+  Primitive* m_primitive;
 };
 
 #endif

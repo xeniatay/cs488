c7c4acc86c8a51f5cea5bfa1378636cf72cc456e
Xenia Tay <xeniatzy@gmail.com>
2014-06-26 00:44:28 -0400

    fixed stupid ray thing

---
+++ handin/A4/src/ray.hpp
@@ -0,0 +1,16 @@
+#ifndef CS488_RAY_HPP
+#define CS488_RAY_HPP
+
+#include "algebra.hpp"
+
+class Ray {
+  public:
+    Ray() : m_dir( Vector3D(0, 0, 0) ), m_origin( Point3D(0, 0, 0) ), hit(false) {};
+    Ray(Vector3D& dir, Point3D& origin) : m_dir( dir ), m_origin( origin ), hit(false) {};
+
+    Vector3D m_dir;
+    Point3D m_origin;
+    bool hit;
+};
+
+#endif

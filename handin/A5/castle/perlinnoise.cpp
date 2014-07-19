58ff59bcc69bd11ebabe480b4537d6ae02fcfe23
Xenia Tay <xeniatzy@gmail.com>
2014-07-18 22:15:22 -0400

    playing with perlinnoise

--- handin/A5/castle/perlinnoise.cpp
+++ handin/A5/castle/perlinnoise.cpp
@@ -17,7 +17,7 @@ double noise_1(double x, double y) {
 }
 
 double smoothed_noise_1(double x, double y) {
-  double corners = ( noise_1(x - 1, y - 1) + noise_1(x + 1, y - 1) + noise_1(x + 1, y + 1) ) / 16;
+  double corners = ( noise_1(x - 1, y - 1) + noise_1(x + 1, y - 1) + noise_1(x - 1, y + 1) + noise_1(x + 1, y + 1) ) / 16;
   double sides = ( noise_1(x - 1, y) + noise_1( x + 1, y) + noise_1(x, y - 1) + noise_1(x, y + 1) ) / 8;
   double center = noise_1(x, y) / 4;
 
@@ -32,7 +32,7 @@ double noise_2(double x, double y) {
 }
 
 double smoothed_noise_2(double x, double y) {
-  double corners = ( noise_2(x - 1, y - 1) + noise_2(x + 1, y - 1) + noise_2(x + 1, y + 1) ) / 16;
+  double corners = ( noise_2(x - 1, y - 1) + noise_2(x + 1, y - 1) + noise_2(x - 1, y + 1) + noise_2(x + 1, y + 1) ) / 16;
   double sides = ( noise_2(x - 1, y) + noise_2( x + 1, y) + noise_2(x, y - 1) + noise_2(x, y + 1) ) / 8;
   double center = noise_2(x, y) / 4;
 
@@ -47,7 +47,7 @@ double noise_3(double x, double y) {
 }
 
 double smoothed_noise_3(double x, double y) {
-  double corners = ( noise_3(x - 1, y - 1) + noise_3(x + 1, y - 1) + noise_3(x + 1, y + 1) ) / 16;
+  double corners = ( noise_3(x - 1, y - 1) + noise_3(x + 1, y - 1) + noise_3(x - 1, y + 1) + noise_3(x + 1, y + 1) ) / 16;
   double sides = ( noise_3(x - 1, y) + noise_3( x + 1, y) + noise_3(x, y - 1) + noise_3(x, y + 1) ) / 8;
   double center = noise_3(x, y) / 4;
 
@@ -62,7 +62,7 @@ double noise_4(double x, double y) {
 }
 
 double smoothed_noise_4(double x, double y) {
-  double corners = ( noise_4(x - 1, y - 1) + noise_4(x + 1, y - 1) + noise_4(x + 1, y + 1) ) / 16;
+  double corners = ( noise_4(x - 1, y - 1) + noise_4(x + 1, y - 1) + noise_4(x - 1, y + 1) + noise_4(x + 1, y + 1) ) / 16;
   double sides = ( noise_4(x - 1, y) + noise_4( x + 1, y) + noise_4(x, y - 1) + noise_4(x, y + 1) ) / 8;
   double center = noise_4(x, y) / 4;
 
@@ -77,7 +77,7 @@ double noise_5(double x, double y) {
 }
 
 double smoothed_noise_5(double x, double y) {
-  double corners = ( noise_5(x - 1, y - 1) + noise_5(x + 1, y - 1) + noise_5(x + 1, y + 1) ) / 16;
+  double corners = ( noise_5(x - 1, y - 1) + noise_5(x + 1, y - 1) + noise_5(x - 1, y + 1) + noise_5(x + 1, y + 1) ) / 16;
   double sides = ( noise_5(x - 1, y) + noise_5( x + 1, y) + noise_5(x, y - 1) + noise_5(x, y + 1) ) / 8;
   double center = noise_5(x, y) / 4;
 
@@ -186,12 +186,12 @@ double interpolate(double a, double b, double x) {
 
 // actual perlin
 double perlin2d(double x, double y) {
-  int num_octaves = 1, j = 0;
+  int num_octaves = 5, j = 0;
   double n = 10, amp = 0, total = 0, freq = 0;
   double freq1 = 2, p1 = 0.25; // persistence
-  double freq2 = 8, p2 = 0.25; // persistence
-  double freq3 = 3, p3 = 0.25; // persistence
-  double freq4 = 4, p4 = 0.5; // persistence
+  double freq2 = 2, p2 = 0.5; // persistence
+  double freq3 = 2, p3 = sqrt(2.0); // persistence
+  double freq4 = 2, p4 = 1; // persistence
   double freq5 = 2, p5 = 0.030; // persistence
 
   for(int i = 1; i <= n; i++) {
